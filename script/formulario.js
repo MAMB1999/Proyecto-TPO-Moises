112aa4be0b61fdfd29917b3841aa79c10427d27e
MAMB1999 <74522304+MAMB1999@users.noreply.github.com>
2021-10-29 23:42:11 -0300

    Agregada logica, commit realizado para subir tpo

--- script/formulario.js
+++ script/formulario.js
@@ -4,4 +4,26 @@ const contrasena = document.getElementById("contrasena");
 const form = document.getElementById("form");
 const parrafo = document.getElementById("notificacion");
 
-form.addEventListener("submit")
+form.addEventListener("submit",valor => {
+    valor.preventDefault()
+    let notificacion = "";
+    entrar = false;
+    //regex extraido de https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    let regex_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(nombre.ariaValueMax.length < 4){
+        notificacion += "El nombre no es valido <br>"
+        entrar = true;
+    }
+    if(!regex_email.test(email.value)){
+        notificacion += "El correo no es valido <br>"
+        entrar = true;
+    }
+    if(contrasena.value.length < 8){
+        notificacion += "La contraseña no es valida <br>"
+        entrar = true;
+    }
+    if(entrar){
+        parrafo.innerHTML(notificacion)
+    }
+})
+
