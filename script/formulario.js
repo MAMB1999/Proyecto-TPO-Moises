eeab9c8b9f10da6877fc73307819bd8a679162aa
MAMB1999 <74522304+MAMB1999@users.noreply.github.com>
2021-10-29 23:17:57 -0300

    formulario realizado, le falta logica

---
+++ script/formulario.js
@@ -0,0 +1,7 @@
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const contrasena = document.getElementById("contrasena");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("notificacion");
+
+form.addEventListener("submit")
